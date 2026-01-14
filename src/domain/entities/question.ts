37a0f15578c5785254bcd74067fe2d7e8000f8ed
Danilo Carlos Marques <marquesdanilocarlos@gmail.com>
2026-01-13 22:30:13 -0300

    Aula 04 - Conceito de Value Object, classe de Slug

--- src/domain/entities/question.ts
+++ src/domain/entities/question.ts
@@ -1,8 +1,10 @@
 import { randomUUID } from 'node:crypto'
+import Slug from '@/domain/entities/value-objects/slug'
 
 type QuestionProps = {
   title: string
   content: string
+  slug: Slug
   authorId: string
 }
 
@@ -10,12 +12,14 @@ export default class Question {
   public id: string
   public title: string
   public content: string
+  public slug: Slug
   public authorId: string
 
   constructor(props: QuestionProps, id?: string) {
     this.title = props.title
     this.content = props.content
     this.id = id ?? randomUUID()
+    this.slug = props.slug
     this.authorId = props.authorId
   }
 }
---
+++ src/tests/slug.test.ts
@@ -0,0 +1,8 @@
+import { expect, test } from 'vitest'
+import Slug from '@/domain/entities/value-objects/slug'
+
+test('Deve ser possível criar um slug a partir de um texto', () => {
+  const slug = Slug.createFromText('Título exemplo de pergunta')
+
+  expect(slug.value).toEqual('titulo-exemplo-de-pergunta')
+})
